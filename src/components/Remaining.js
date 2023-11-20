bfa6097b85e651cf7dbf5ec1c299155d080e633e
Dario Pratola <dario.pratola@gmail.com>
2023-11-20 15:48:06 -0500

    budget_morethan_spending

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,10 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 const Remaining = () => {
     const { expenses, budget } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total = total + item.cost);
     }, 0);
+
+    useEffect(() => {
+        // Mostra un alert quando totalExpenses supera il budget
+        if (totalExpenses > budget) {
+          window.alert('You cannot reduce the budget value lower than the spending');
+        }
+      }, [totalExpenses, budget]);
     const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
     return (
         <div className={`alert ${alertType}`}>
