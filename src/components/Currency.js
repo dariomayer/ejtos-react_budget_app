b28ec5138f0f4402f08ff7e8196d953337dad2d6
Dario <dario.pratola@gmail.com>
2023-11-21 15:25:49 -0500

    create a budget allocation application

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,28 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+import { Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
 
 const Currency = () => {
-  const {dispatch } = useContext(AppContext);
+    const {dispatch, currency } = useContext(AppContext);
+  const [selectedCurrency, setSelectedCurrency] = useState(currency);
 
-    const changeCurrency = (val)=>{
-            dispatch({
-                type: 'CHG_CURRENCY',
-                payload: val,
-            })
-    }
-    
+  const changeCurrency = (val, event) => {
+    setSelectedCurrency(event.target.innerText);
+    dispatch({
+      type: 'CHG_CURRENCY',
+      payload: val,
+    })
+  }
 
-    return (
-        <div style={{backgroundColor: 'green', width: '200px', padding: '10px'}}> 
-            <select style={{width: '100%', color: 'white', backgroundColor: 'green', border: 'none', appearance: 'none'}} name="Currency" id="Currency" onChange={event=>changeCurrency(event.target.value)}>
-                <option value="$">Dollar($)</option>
-                <option value="£">Pound(£)</option>
-                <option value="€">Euro(€)</option>
-                <option value="₹">Rupee(₹)</option>
-            </select>	
-        </div>
-    );
-    
+  return (
+    <div>
+      <Dropdown id="dropdown-basic" onSelect={changeCurrency}>
+        <Dropdown.Toggle>
+        {selectedCurrency ? `Currency (${selectedCurrency})` : 'Currency ()'}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item eventKey="$">$ Dollar</Dropdown.Item>
+          <Dropdown.Item eventKey="£">£ Pound</Dropdown.Item>
+          <Dropdown.Item eventKey="€">€ Euro</Dropdown.Item>
+          <Dropdown.Item eventKey="₹">₹ Rupee</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
 };
 
 export default Currency;
